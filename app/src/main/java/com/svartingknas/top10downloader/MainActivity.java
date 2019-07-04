7d8978c3d9cf74ae27e382ab1bbd0f672ca3e1ef
svartingknas <svartingknas@gmail.com>
2019-07-03 22:44:52 -0600

    update dependencies

--- app/src/main/java/com/svartingknas/top10downloader/MainActivity.java
+++ app/src/main/java/com/svartingknas/top10downloader/MainActivity.java
@@ -5,10 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
